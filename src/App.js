200b946d7730dd704f752dfaba431ed77bbcca05
AbhayRana14 <14abhayrana@gmail.com>
2021-05-12 23:25:01 +0530

    green tag

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import TodoList from './components/TodoList';
-
+//this is new sort of code and needs to be practiced again after some time
 function App() {
   return (
     <div className="todo-app">
